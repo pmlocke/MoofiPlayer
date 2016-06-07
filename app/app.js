94ebcbfa2759fad933eabe6a653d639917a0666c
plocke <patrickl@slalom.com>
2016-06-06 22:57:43 -0500

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version',
+  "ngSanitize",
+  "com.2fdevs.videogular",
+  "com.2fdevs.videogular.plugins.controls",
+  "com.2fdevs.videogular.plugins.overlayplay",
+  "com.2fdevs.videogular.plugins.poster"
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ app/view1/view1.js
@@ -0,0 +1,145 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+    .constant('_', window._)
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'AudioCtrl'
+        });
+    }])
+    .controller('AudioCtrl', ["$sce", "$http", "$scope", function ($sce, $http, $scope) {
+        var ctrl = this;
+        ctrl.player1 = null;
+        ctrl.player2 = null;
+        ctrl.stateCheckTime = 30; //Ask for Habitat state starting @ 30 seconds from end of track
+        ctrl.fadeTime = 15; //Begin crossfading @ 15 seconds
+        ctrl.color = [0, 0, 0];
+        ctrl.lockState = false; //True: Currently looking or have found Habitat State. Reset when tracks transition
+        ctrl.fadeIn = null;
+        ctrl.fadeOut = null;
+        ctrl.vibes = null;
+        ctrl.nextVibe = null;
+
+        this.config = {
+            source1: [
+                {
+                    src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),
+                    type: "video/mp4"
+                }
+            ],
+            source2: [
+                {
+                    src: $sce.trustAsResourceUrl("http://0.0.0.0:3000/Milky Chance - Stolen Dance (Alex Brandt's Saxual Edit).mp3"),
+                    type: "audio/mp3"
+                }
+            ],
+            theme: "bower_components/videogular-themes-default/videogular.css",
+            plugins: {
+                poster: "http://www.videogular.com/assets/images/videogular.png"
+            }
+        };
+
+        ctrl.init = function () {
+            var r = {playIndex: 0, tracks: []};
+            var g = {playIndex: 0, tracks: []};
+            var b = {playIndex: 0, tracks: []};
+            ctrl.vibes = {"red": r, "green": g, "blue": b};
+
+            ctrl.getTracklist("red", r);
+            ctrl.getTracklist("green", g);
+            ctrl.getTracklist("blue", b);
+
+        };
+
+        ctrl.onPlayer1Ready = function (API) {
+            ctrl.player1 = API;
+            ctrl.player1.setVolume(1);
+        };
+
+        ctrl.onPlayer2Ready = function (API) {
+            ctrl.player2 = API;
+            ctrl.player2.setVolume(1);
+        };
+
+        ctrl.onTick = function (currentTime, duration) {
+            var remaining = duration - currentTime;
+
+            if (remaining <= ctrl.stateCheckTime && !ctrl.lockState) {
+                ctrl.getVibe();
+            }
+
+            if (remaining <= ctrl.fadeTime && remaining > 0) {
+                ctrl.crossfade(remaining);
+            }
+        };
+
+        ctrl.crossfade = function (remaining) {
+            if (ctrl.player1.currentState != "play") {
+                ctrl.player1.setVolume(0);
+                ctrl.player1.changeSource(ctrl.nextVibe.tracks);
+                ctrl.player1.play();
+                ctrl.fadeIn = ctrl.player1;
+                ctrl.fadeOut = ctrl.player2;
+            }
+
+            if (ctrl.player2.currentState != "play") {
+                ctrl.player2.setVolume(0);
+                ctrl.player2.changeSource(ctrl.nextVibe.tracks);
+                ctrl.player2.play();
+                ctrl.fadeIn = ctrl.player2;
+                ctrl.fadeOut = ctrl.player1;
+            }
+
+            ctrl.fadeIn.setVolume(1 - remaining / ctrl.fadeTime);
+            ctrl.fadeOut.setVolume(remaining / ctrl.fadeTime);
+
+        };
+
+        ctrl.onComplete = function () {
+            if (ctrl.fadeIn != null & ctrl.fadeOut != null) {
+                ctrl.fadeIn.setVolume(1);
+                ctrl.fadeOut.setVolume(0);
+                ctrl.fadeOut.stop();
+                ctrl.fadeIn = null;
+                ctrl.fadeOut = null;
+                ctrl.lockState = false;
+            }
+        };
+
+        //HTTP Methods
+        ctrl.getTracklist = function (listName, vibe) {
+            var host = "http://0.0.0.0:3000/";
+            $http.get(host + listName, {headers: "Accept:application/json"}).then(function (response) {
+                var sourceList = [];
+
+                _(response).forEach(function (value) {
+                    sourceList.push({src: $sce.trustAsResourceUrl(host + value), type: "audio/mp3"});
+                });
+
+                _.shuffle(sourceList);
+
+                vibe.tracks = sourceList;
+            });
+        };
+
+        ctrl.getVibe = function () {
+            ctrl.lockVibe = true;
+            var uri = "http://habitat.local/vibe";
+            $http.get(uri).then(function (response) {
+                    ctrl.nextVibe = ctrl.vibes[response];
+                },
+                function (error) {
+                    ctrl.nextVibe = ctrl.randomVibe();
+                }
+            );
+        };
+
+        ctrl.randomVibe = function () {
+            var keys = Object.keys(ctrl.vibes);
+            var key = keys[Math.floor((Math.random() * keys.length))];
+            return ctrl.vibes[key];
+        };
+
+        ctrl.init();
+    }]);
